cdf2969e85c218519634068685fab09dd4b452ca
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2021-06-14 18:06:25 -0300

    Use laravel-mix-eslint

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,11 @@
 const path = require('path')
 const mix = require('laravel-mix')
 
+require('laravel-mix-eslint')
+
 mix.setPublicPath('dist')
   .js('resources/js/tallkit.js', 'dist')
+  .eslint({ fix: true })
   .sourceMaps()
   .version()
   .webpackConfig({
