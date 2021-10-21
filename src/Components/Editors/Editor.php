a73fd4703f1af6c0cd1c3f0b22d7e864a8d99f91
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2021-10-21 15:59:58 -0300

    Refactor editors

---
+++ src/Components/Editors/Editor.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace TALLKit\Components\Editors;
+
+use TALLKit\Components\Forms\Textarea;
+
+abstract class Editor extends Textarea
+{
+    /**
+     * @var array
+     */
+    public $options;
+
+    /**
+     * Create a new component instance.
+     *
+     * @param  string|null  $name
+     * @param  string|null  $id
+     * @param  string|bool|null  $label
+     * @param  mixed  $bind
+     * @param  mixed  $default
+     * @param  string|null  $language
+     * @param  bool  $showErrors
+     * @param  string|null  $theme
+     * @param  array  $options
+     * @return void
+     */
+    public function __construct(
+        $name = null,
+        $id = null,
+        $label = null,
+        $bind = null,
+        $default = null,
+        $language = null,
+        $showErrors = true,
+        $theme = null,
+        $options = []
+    ) {
+        parent::__construct(
+            $name,
+            $id,
+            $label,
+            $bind,
+            $default,
+            $language,
+            $showErrors,
+            $theme,
+            false
+        );
+
+        $this->options = $options;
+    }
+
+    /**
+     * Json options.
+     *
+     * @return string
+     */
+    public function jsonOptions()
+    {
+        return json_encode((object) array_replace_recursive(
+            $this->themeProvider->options->getAttributes(),
+            $this->options
+        ));
+    }
+}
--- src/Components/Editors/Trix.php
+++ src/Components/Editors/Trix.php
@@ -2,44 +2,7 @@
 
 namespace TALLKit\Components\Editors;
 
-use Illuminate\Support\Str;
-use TALLKit\Components\Forms\Textarea;
-
-class Trix extends Textarea
+class Trix extends Editor
 {
-    /**
-     * Create a new component instance.
-     *
-     * @param  string|null  $name
-     * @param  string|null  $id
-     * @param  string|bool|null  $label
-     * @param  mixed  $bind
-     * @param  mixed  $default
-     * @param  string|null  $language
-     * @param  bool  $showErrors
-     * @param  string|null  $theme
-     * @return void
-     */
-    public function __construct(
-        $name = null,
-        $id = null,
-        $label = null,
-        $bind = null,
-        $default = null,
-        $language = null,
-        $showErrors = true,
-        $theme = null
-    ) {
-        parent::__construct(
-            $name,
-            $id ?: $name ?: Str::random(),
-            $label,
-            $bind,
-            $default,
-            $language,
-            $showErrors,
-            $theme,
-            false
-        );
-    }
+    //
 }
