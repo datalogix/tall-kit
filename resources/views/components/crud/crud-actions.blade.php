cc135a3ef7253d86be11dc9d4ca71fa25286321c
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2021-11-26 15:01:37 -0300

    Use tooltip

--- resources/views/components/crud/crud-actions.blade.php
+++ resources/views/components/crud/crud-actions.blade.php
@@ -18,55 +18,73 @@
 @endforeach
 
 @if (! in_array($routeName, ['show', 'view']) && $route = route_detect([$prefix.'.show', $prefix.'.view'], $parameters, null))
-    <x-button
-        {{ $attributes->mergeOnlyThemeProvider($themeProvider, 'show') }}
-        preset="show"
-        :href="$route"
-        :theme="$theme"
-    />
+    <x-tooltip value="Show">
+        <x-button
+            {{ $attributes->mergeOnlyThemeProvider($themeProvider, 'show') }}
+            preset="show"
+            text=""
+            :href="$route"
+            :theme="$theme"
+        />
+    </x-tooltip>
 @endif
 
 @if (! in_array($routeName, ['edit', 'update', 'form']) && $route = route_detect([$prefix.'.edit', $prefix.'.update', $prefix.'.form'], $parameters, null))
-    <x-button
-        {{ $attributes->mergeOnlyThemeProvider($themeProvider, 'edit') }}
-        preset="edit"
-        :href="$route"
-        :theme="$theme"
-    />
+    <x-tooltip value="Edit">
+        <x-button
+            {{ $attributes->mergeOnlyThemeProvider($themeProvider, 'edit') }}
+            preset="edit"
+            text=""
+            :href="$route"
+            :theme="$theme"
+        />
+    </x-tooltip>
 @endif
 
 @if (! in_array($routeName, ['copy', 'duplicate', 'clone']) && $route = route_detect([$prefix.'.copy', $prefix.'.duplicate', $prefix.'.clone'], $parameters, null))
-    <x-form-button
-        {{ $attributes->mergeOnlyThemeProvider($themeProvider, 'copy') }}
-        preset="copy"
-        :action="$route"
-        :theme="$theme"
-    />
+    <x-tooltip value="Copy">
+        <x-form-button
+            {{ $attributes->mergeOnlyThemeProvider($themeProvider, 'copy') }}
+            preset="copy"
+            text=""
+            :action="$route"
+            :theme="$theme"
+        />
+    </x-tooltip>
 @endif
 
 @if (! in_array($routeName, ['up', 'move-up']) && $route = route_detect([$prefix.'.up', $prefix.'.move-up'], $parameters, null))
-    <x-form-button
-        {{ $attributes->mergeOnlyThemeProvider($themeProvider, 'move-up') }}
-        preset="move-up"
-        :action="$route"
-        :theme="$theme"
-    />
+    <x-tooltip value="Move up">
+        <x-form-button
+            {{ $attributes->mergeOnlyThemeProvider($themeProvider, 'move-up') }}
+            preset="move-up"
+            text=""
+            :action="$route"
+            :theme="$theme"
+        />
+    </x-tooltip>
 @endif
 
 @if (! in_array($routeName, ['down', 'move-down']) && $route = route_detect([$prefix.'.down', $prefix.'.move-down'], $parameters, null))
-    <x-form-button
-        {{ $attributes->mergeOnlyThemeProvider($themeProvider, 'move-down') }}
-        preset="move-down"
-        :action="$route"
-        :theme="$theme"
-    />
+    <x-tooltip value="Move down">
+        <x-form-button
+            {{ $attributes->mergeOnlyThemeProvider($themeProvider, 'move-down') }}
+            preset="move-down"
+            text=""
+            :action="$route"
+            :theme="$theme"
+        />
+    </x-tooltip>
 @endif
 
 @if (! in_array($routeName, ['destroy', 'exclude']) && $route = route_detect([$prefix.'.destroy', $prefix.'.exclude'], $parameters, null))
-    <x-form-button
-        {{ $attributes->mergeOnlyThemeProvider($themeProvider, 'destroy') }}
-        preset="delete"
-        :action="$route"
-        :theme="$theme"
-    />
+    <x-tooltip value="Delete">
+        <x-form-button
+            {{ $attributes->mergeOnlyThemeProvider($themeProvider, 'destroy') }}
+            preset="delete"
+            text=""
+            :action="$route"
+            :theme="$theme"
+        />
+    </x-tooltip>
 @endif
