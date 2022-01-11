5e9d554760541b71f70e956fadac8807c082ec2b
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2022-01-11 14:15:34 -0300

    Add `Turbo` component

---
+++ resources/views/components/layouts/turbo.blade.php
@@ -0,0 +1,1 @@
+<script src="{{ config('tallkit.assets.turbo') }}"></script>
---
+++ src/Components/Layouts/Turbo.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace TALLKit\Components\Layouts;
+
+use TALLKit\Components\BladeComponent;
+
+class Turbo extends BladeComponent
+{
+    //
+}
