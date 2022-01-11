590427c7ca40fa91fe0406aea11d086f1ce9a585
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2022-01-11 14:22:52 -0300

    Use `alias` helper

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,12 @@
-const path = require('path')
 const mix = require('laravel-mix')
 const ESLintPlugin = require('eslint-webpack-plugin')
 
 mix.setPublicPath('dist')
+  .alias({ '@': 'resources/js' })
   .js('resources/js/tallkit.js', 'dist')
   .sourceMaps()
   .version()
   .webpackConfig({
-    resolve: {
-      symlinks: false,
-      alias: {
-        '@': path.resolve(__dirname, 'resources/js/')
-      }
-    },
     plugins: [
       new ESLintPlugin({
         extensions: ['js'],
