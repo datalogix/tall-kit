63784b8a02156d9ce7305277a926b76c8ef7abe0
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2022-01-24 10:02:33 -0300

    Handle `tooltip`with `[data-tooltip]`

--- resources/js/components/overlays/tooltip.js
+++ resources/js/components/overlays/tooltip.js
@@ -1,9 +1,21 @@
-export default ({ loadComponentAssets }) => ({
+import { loadComponentAssets } from '@/utils'
+
+export default () => ({
   tippy: null,
 
   async setup (options) {
     await loadComponentAssets('tooltip')
 
     this.tippy = window.tippy(this.$refs.root ? this.$refs.root : this.$el, options)
+  }
+})
+
+window.addEventListener('tallkit:load', async () => {
+  const elements = document.querySelectorAll('[data-tooltip]')
+
+  if (elements.length) {
+    await loadComponentAssets('tooltip')
+
+    window.tippy(elements)
   }
 })
