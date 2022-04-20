73165da33e1777c706ae2da047e7ebfc4e110026
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2022-04-20 16:56:10 -0300

    Fix crossorigin

--- resources/views/components/scripts/google-fonts.blade.php
+++ resources/views/components/scripts/google-fonts.blade.php
@@ -1,7 +1,7 @@
 @if ($noscript)
     <noscript><link rel="stylesheet" href="{{ $url }}" /></noscript>
 @else
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
     <link rel="preload" as="style" href="{{ $url }}" />
     <link rel="stylesheet" href="{{ $url }}" media="print" onload="this.media='all'" />
 @endif
