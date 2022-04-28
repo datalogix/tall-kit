44c793347520d598cbd95048c2b3e58beb42a9e9
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2022-04-28 14:42:03 -0300

    Hide checkbox list if empty options

--- resources/views/components/forms/checkbox-list.blade.php
+++ resources/views/components/forms/checkbox-list.blade.php
@@ -1,22 +1,24 @@
-<x-group
-    {{ $attributes->mergeThemeProvider($themeProvider, 'container') }}
-    :name="$name"
-    :label="$label"
-    :inline="$inline"
-    :grid="$grid"
-    :fieldset="$fieldset"
-    :show-errors="$showErrors"
-    :theme="$theme"
->
-    @foreach ($options as $key => $option)
-        <x-checkbox
-            {{ $attributes->mergeOnlyThemeProvider($themeProvider, 'checkbox') }}
-            :name="$name"
-            :label="$option"
-            :value="$key"
-            :bind="$bind"
-            :show-errors="false"
-            :theme="$theme"
-        />
-    @endforeach
-</x-group>
+@if ($options->isNotEmpty())
+    <x-group
+        {{ $attributes->mergeThemeProvider($themeProvider, 'container') }}
+        :name="$name"
+        :label="$label"
+        :inline="$inline"
+        :grid="$grid"
+        :fieldset="$fieldset"
+        :show-errors="$showErrors"
+        :theme="$theme"
+    >
+        @foreach ($options as $key => $option)
+            <x-checkbox
+                {{ $attributes->mergeOnlyThemeProvider($themeProvider, 'checkbox') }}
+                :name="$name"
+                :label="$option"
+                :value="$key"
+                :bind="$bind"
+                :show-errors="false"
+                :theme="$theme"
+            />
+        @endforeach
+    </x-group>
+@endif
