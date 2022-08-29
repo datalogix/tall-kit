f90725d5e5a3bd8177c4c886b08df9e5926808a9
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2022-08-29 14:52:11 -0300

    Accept `name` or `icon` property

--- src/Components/Icons/Iconify.php
+++ src/Components/Icons/Iconify.php
@@ -15,15 +15,17 @@ class Iconify extends BladeComponent
      * Create a new component instance.
      *
      * @param  string|null  $icon
+     * @param  string|null  $name
      * @param  string|null  $theme
      * @return void
      */
     public function __construct(
         $icon = null,
+        $name = null,
         $theme = null
     ) {
         parent::__construct($theme);
 
-        $this->icon = $icon;
+        $this->icon = $icon ?? $name;
     }
 }
