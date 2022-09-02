cc62d0bd4cda9d471a0c577824d29785c8199e04
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2022-09-02 16:04:10 -0300

    Fill form from x-model

--- resources/js/components/forms/form.js
+++ resources/js/components/forms/form.js
@@ -2,9 +2,11 @@ export default ({ loadable }) => ({
   ...loadable(),
 
   confirm: null,
+  form: {},
 
-  setup (confirm) {
+  setup (confirm = null) {
     this.confirm = confirm
+    this.form = this.fillForm()
   },
 
   prepareSubmit (event) {
@@ -19,5 +21,26 @@ export default ({ loadable }) => ({
     }
 
     return event.preventDefault()
+  },
+
+  fillForm () {
+    const form = {}
+    const el = this.$refs.root ? this.$refs.root : this.$el
+
+    Array.from(el.querySelectorAll('input,select,textarea'))
+      .filter((element) => element.getAttributeNames().filter((attr) => attr.startsWith('x-model')).length)
+      .forEach((element) => {
+        const attr = element.getAttribute(element.getAttributeNames().find((attr) => attr.startsWith('x-model')) || 'name')
+        const parts = attr.replace('form.', '').replace(/\[/g, '.').replace(/\]/g, '').split('.')
+        const last = parts.pop()
+
+        parts.reduce(
+        // eslint-disable-next-line no-return-assign
+          (o, k, i, kk) => o[k] = o[k] || (isFinite(i + 1 in kk ? kk[i + 1] : last) ? [] : {}),
+          form
+        )[last] = element.value
+      })
+
+    return form
   }
 })
