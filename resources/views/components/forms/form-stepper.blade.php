e80e6347526d01baa3fc18cc193d97efab6e9588
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2022-09-28 15:44:00 -0300

    Add wrap actions

--- resources/views/components/forms/form-stepper.blade.php
+++ resources/views/components/forms/form-stepper.blade.php
@@ -54,35 +54,44 @@
                     @isset ($actions)
                         {{ $actions }}
                     @else
-                        @if ($prev && $current > 1)
-                            <x-button {{
-                                $attributes
-                                    ->mergeOnlyThemeProvider($themeProvider, 'prev')
-                                    ->mergeOnlyThemeProvider($themeProvider, $mode, 'prev')
-                                    ->mergeOnlyThemeProvider($themeProvider, $isWired() ? 'prev-wire' : null)
-                                }}
-                                preset="prev"
-                                :theme="$theme"
-                            />
-                        @endif
+                        <div {{
+                            $attributes
+                                ->mergeOnlyThemeProvider($themeProvider, 'actions')
+                                ->mergeOnlyThemeProvider($themeProvider, $mode, 'actions')
+                                ->mergeOnlyThemeProvider($themeProvider, $isWired() ? 'wire' : null, 'actions')
+                        }}>
+                            @if ($prev)
+                                <x-button {{
+                                    $attributes
+                                        ->mergeOnlyThemeProvider($themeProvider, 'prev')
+                                        ->mergeOnlyThemeProvider($themeProvider, $mode, 'prev')
+                                        ->mergeOnlyThemeProvider($themeProvider, $isWired() ? 'wire' : null, 'prev')
+                                        ->merge($current > 1 ? [] : ['style' => 'display:none;'])
+                                    }}
+                                    preset="prev"
+                                    :theme="$theme"
+                                />
+                            @endif
 
-                        @if ($next)
-                            <x-submit {{
-                                $attributes
-                                    ->mergeOnlyThemeProvider($themeProvider, 'next')
-                                    ->mergeOnlyThemeProvider($themeProvider, $mode, 'next')
-                                    ->mergeOnlyThemeProvider($themeProvider, $isWired() ? 'next-wire' : null)
-                                }}
-                                preset="next"
-                                :theme="$theme"
-                            />
-                        @endif
+                            @if ($next)
+                                <x-submit {{
+                                    $attributes
+                                        ->mergeOnlyThemeProvider($themeProvider, 'next')
+                                        ->mergeOnlyThemeProvider($themeProvider, $mode, 'next')
+                                        ->mergeOnlyThemeProvider($themeProvider, $isWired() ? 'wire' : null, 'next')
+                                    }}
+                                    preset="next"
+                                    :theme="$theme"
+                                />
+                            @endif
+                        </div>
 
                         @if ($loading && $isWired())
                             <div {{
                                 $attributes
                                     ->mergeOnlyThemeProvider($themeProvider, 'loading')
                                     ->mergeOnlyThemeProvider($themeProvider, $mode, 'loading')
+                                    ->mergeOnlyThemeProvider($themeProvider, $isWired() ? 'wire' : null, 'loading')
                                 }}
                             >
                                 <x-loading />
