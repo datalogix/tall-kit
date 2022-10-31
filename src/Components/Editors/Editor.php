63533d7c3172ed5152ef3ae077b874a4cd024ee7
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2022-10-31 17:36:29 -0300

    Fix editor options

--- src/Components/Editors/Editor.php
+++ src/Components/Editors/Editor.php
@@ -2,6 +2,8 @@
 
 namespace TALLKit\Components\Editors;
 
+use Illuminate\Support\Str;
+
 class Editor extends AbstractEditor
 {
     /**
@@ -12,5 +14,17 @@ class Editor extends AbstractEditor
     protected function getComponentKey()
     {
         return $this->provider ?? 'tinymce';
+    }
+
+    /**
+     * Json options.
+     *
+     * @return string
+     */
+    public function jsonOptions(...$args)
+    {
+        $editor = app(__NAMESPACE__.'\\'.Str::studly($this->getComponentKey()));
+
+        return $editor->jsonOptions(...$args);
     }
 }
