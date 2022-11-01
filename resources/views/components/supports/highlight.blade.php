fde03acd5527ce2b1b611433e07e7405092ddfcd
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2022-11-01 15:38:10 -0300

    Add `code` parameter

--- resources/views/components/supports/highlight.blade.php
+++ resources/views/components/supports/highlight.blade.php
@@ -1,5 +1,9 @@
 <div {{ $attributes->mergeOnlyThemeProvider($themeProvider, 'container') }}>
     <x-loading {{ $attributes->mergeOnlyThemeProvider($themeProvider, 'loading') }} />
 
-    <pre><code {{ $attributes->mergeThemeProvider($themeProvider, 'highlight') }}>{{ $slot }}</code></pre>
+    <pre><!--
+        --><code {{ $attributes->mergeThemeProvider($themeProvider, 'highlight') }}><!--
+            -->{{ $slot->isEmpty() ? $code : $slot }}<!--
+        --></code><!--
+    --></pre>
 </div>
--- src/Components/Supports/Highlight.php
+++ src/Components/Supports/Highlight.php
@@ -6,5 +6,24 @@ use TALLKit\Components\BladeComponent;
 
 class Highlight extends BladeComponent
 {
-    //
+    /**
+     * @var string|null
+     */
+    public $code;
+
+    /**
+     * Create a new component instance.
+     *
+     * @param  string|null  $code
+     * @param  string|null  $theme
+     * @return void
+     */
+    public function __construct(
+        $code = null,
+        $theme = null
+    ) {
+        parent::__construct($theme);
+
+        $this->code = $code;
+    }
 }
